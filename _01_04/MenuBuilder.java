e88eac8de9252c41e5eda2cffbc4d1d859af2d33
Nonku-Mzinyane <nonkupn.mchunu@gmail.com>
2025-06-04 15:14:49 +0200

    Update MenuBuilder.java

--- _01_04/MenuBuilder.java
+++ _01_04/MenuBuilder.java
@@ -7,7 +7,7 @@ public class MenuBuilder {
 
     // Create a variable called menuTitle of type String and assign it the value "My
     // Dream Menu:".
-
+string menuTitle = "My dream menu";
     // Print the menuTitle variable to the console.
 
     // Create a variable called menu of type ArrayList.
